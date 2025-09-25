7492ff39a9485b4662eba9cca425d057aaff0b0f
mattiviera <matiiviera38@gmail.com>
2025-09-25 14:50:45 -0300

    correcciones para pasar test

--- app/main.py
+++ app/main.py
@@ -3,32 +3,30 @@ from app.conversor import (
     celsius_a_fahrenheit,
     fahrenheit_a_celsius,
     km_a_millas,
-    millas_a_km,
+    millas_a_km
 )
 
-app = FastAPI()
+app = FastAPI(title="Conversor de Unidades API")
 
-
+# Root
 @app.get("/")
 def root():
-    return {"message": "Conversor API funcionando"}
+    return {"message": "Bienvenido al conversor de unidades"}
 
-
-@app.get("/celsius_to_fahrenheit/{c}")
+# Temperatura
+@app.get("/celsius-to-fahrenheit/{c}")
 def api_celsius_to_fahrenheit(c: float):
     return {"fahrenheit": celsius_a_fahrenheit(c)}
 
-
-@app.get("/fahrenheit_to_celsius/{f}")
+@app.get("/fahrenheit-to-celsius/{f}")
 def api_fahrenheit_to_celsius(f: float):
     return {"celsius": fahrenheit_a_celsius(f)}
 
-
-@app.get("/km_to_miles/{km}")
+# Distancia
+@app.get("/km-to-miles/{km}")
 def api_km_to_miles(km: float):
     return {"miles": km_a_millas(km)}
 
-
-@app.get("/miles_to_km/{miles}")
+@app.get("/miles-to-km/{miles}")
 def api_miles_to_km(miles: float):
     return {"km": millas_a_km(miles)}
